b8f5b942211b58b403f77db20731eb22f488f0a1
Mariya Batanova <batanova708@gmail.com>
2023-02-02 23:20:21 +0200

    Complete

--- index.js
+++ index.js
@@ -2,28 +2,43 @@
 // Бажано одинм рядком, використовуючи деструктуризацію.
 
 const user = {
-    id:1,
-    privateInfo:{
-      fname:"Brad",
-      sname:"Pitt",
-      bday:{
-        day:18,
-        month:12,
-        year:1963,
-      },
-      children:['Helen','Alex','Georg','Anna'],
+  id: 1,
+  privateInfo: {
+    firstName: "Brad",
+    secondName: "Pitt",
+    bday: {
+      day: 18,
+      month: 12,
+      year: 1963,
     },
-    contactInfo:{
-      phone:{
-        work:'123-12-45',
-        mobile:'005-002-003',
-      },
-      adress:{
-        town:'ZP',
-        street:'12Avenu',
-        house:45,
-      },
-      mail:'brad@gmail.com'
+    children: ["Helen", "Alex", "Georg", "Anna"],
+  },
+  contactInfo: {
+    phone: {
+      work: "123-12-45",
+      mobile: "005-002-003",
     },
-    profession:'actor'
+    address: {
+      town: "ZP",
+      street: "12Avenu",
+      house: 45,
+    },
+    mail: "brad@gmail.com",
+  },
+  profession: "actor",
+};
+
+const {
+  privateInfo: {
+    bday: { year: userDateOfBorn },
+    children: [firstChildren,secondChildren,...rest],
+  },
+  contactInfo:{
+    phone:{work:workPhone}
   }
+} = user;
+
+console.log(userDateOfBorn);
+console.log(secondChildren);
+console.log(workPhone);
+
